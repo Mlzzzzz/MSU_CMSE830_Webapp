0ce68dc721f0996a9fcd0d1f350dc101cf05e309
Mlzzzzz <124154467+Mlzzzzz@users.noreply.github.com>
2023-09-18 23:25:58 -0400

    Update Iris_LinqingMo.py

--- Iris_LinqingMo.py
+++ Iris_LinqingMo.py
@@ -1,10 +1,19 @@
 import streamlit as st
 import seaborn as sns
 import plotly.express as px
+import os
+
+is_on_heroku = 'DYNO' in os.environ  
+if is_on_heroku:
+    port = int(os.environ.get('PORT', 8501))
+    os.environ['PORT'] = str(port)
+    os.environ['SERVER_PORT'] = str(port)
+else:
+    os.environ['BROWSER_GATHER_USAGE_STATS'] = 'False'
 
 iris_df = sns.load_dataset("iris")
 st.write("""
-# Iris Dataset Visualization
+# Iris Dataset Visualization - Linqing Mo
 The classic Iris dataset has four features: sepal length, sepal width, petal length, and petal width.
     This app visualizes the dataset in an interactive 3D scatter plot, in which the size of a dot represents the petal width of an Iris flower.
 """)
