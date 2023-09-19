717f47d4c9740afd77bab1e53dc24b1934ddfa26
Mlzzzzz <124154467+Mlzzzzz@users.noreply.github.com>
2023-09-18 22:05:38 -0400

    Add files via upload

---
+++ Iris_LinqingMo.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import seaborn as sns
+import plotly.express as px
+
+iris_df = sns.load_dataset("iris")
+st.write("""
+# Iris Dataset Visualization
+The classic Iris dataset has four features: sepal length, sepal width, petal length, and petal width.
+    This app visualizes the dataset in an interactive 3D scatter plot.
+""")
+fig = px.scatter_3d(iris_df, 
+                    x='sepal_length', 
+                    y='sepal_width', 
+                    z='petal_length', 
+                    color='species',
+                    size='petal_width',
+                    hover_data=['species'])
+    
+st.plotly_chart(fig)
